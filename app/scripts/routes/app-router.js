d9199950caf50f6424f8a7055b1993f6e9bb9f47
SengMing Tan <tansengming@gmail.com>
2013-02-02 19:16:23 +1100

    moves models to own file

--- app/scripts/routes/app-router.js
+++ app/scripts/routes/app-router.js
@@ -11,19 +11,3 @@ App.TablesRoute = Ember.Route.extend({
 });
 
 App.TablesController = Ember.ArrayController.extend();
-
-App.Store = DS.Store.extend({
-  revision: 11,
-  adapter: 'DS.FixtureAdapter'
-});
-
-App.Table = DS.Model.extend();
-
-App.Table.FIXTURES = [
-  {id: 1},
-  {id: 2},
-  {id: 3},
-  {id: 4},
-  {id: 5},
-  {id: 6}
-];
