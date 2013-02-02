dd7e3243d138f572c86cd1cdecdd53362eda88ed
SengMing Tan <tansengming@gmail.com>
2013-02-02 17:23:20 +1100

    adds data and /tables

--- app/scripts/routes/app-router.js
+++ app/scripts/routes/app-router.js
@@ -1,4 +1,32 @@
-// Ember-Start.Router = Ember.Router.extend({
+App.Router.map(function() {
+  this.resource('tables');
+});
+
+App.TablesRoute = Ember.Route.extend({
+  model: function() {
+    return App.Table.find();
+  }
+});
+
+App.TablesController = Ember.ArrayController.extend();
+
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Table = DS.Model.extend();
+
+App.Table.FIXTURES = [
+  {id: 1},
+  {id: 2},
+  {id: 3},
+  {id: 4},
+  {id: 5},
+  {id: 6}
+];
+
+// App.Router = Ember.Router.extend({
 //   root: Ember.Route.extend({
 //     index: Ember.Route.extend({
 //       route: '/'
